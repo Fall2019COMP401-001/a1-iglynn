8e2faeae21136784eadadb6fea6752bb5cc85bc9
faloi <faloi@192.168.1.12>
2019-08-30 11:27:34 -0400

    almost works, just needs final refinement

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,34 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int number = scan.nextInt();
+		
+		//Counts the number of people and puts them in an array.
+		
+		for (int i = 0; i<number; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			String name = (first.charAt(0) + ". " + last);
+			System.out.println(name);
+			//Creates and prints the person's name.
+			
+			int groc = scan.nextInt();
+			System.out.println(groc);
+			//counts the number of items they purchased
+			double total = 0;
+			
+				for (int g = 0; g<groc; g++) {
+					int count = scan.nextInt();
+					String item = scan.next();
+					double price = scan.nextDouble();
+					double itotal = count * price;
+					total = total + itotal;
+			}
+			String ftotal = String.format("%.2f", total);
+			System.out.println(ftotal);
+		}
+		scan.close();
+		System.out.println("Closing...");
 		
 	}
 }
