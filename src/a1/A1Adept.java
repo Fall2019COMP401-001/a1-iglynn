4667a8d5350a857d92b51fae673da396d3743e55
faloi <faloi@192.168.1.12>
2019-08-31 11:43:37 -0400

    on the way, just saving

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,14 +1,29 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		//sets up scanner
+		
+		Map<String, Double> shop = new HashMap<>();
+		//sets up the hashmap for binding the items to their prices
+		
+		int list = scan.nextInt();
+		//gets the number of items in the shop
+		
+		for (int p = 0; p < list; p++) {
+			shop.put(scan.next(), scan.nextDouble());
+		}
+		//binds each item to its price
+		
+		int people = scan.nextInt();
+		System.out.println(people);
 		
 	}
 }
